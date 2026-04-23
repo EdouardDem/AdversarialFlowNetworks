4f8f8cd00fea53d77395505a9914b5d5748709da
Edouard Demotes <e.demotes@gmail.com>
2026-04-23 15:32:28 -0400

    feat: load environment variables from .env file in main function

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from omegaconf import DictConfig
 from src.utils import instantiate
 from src.tb import train
 
+from dotenv import load_dotenv
+
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="ttt.yaml")
 def main(cfg: DictConfig) -> None:
@@ -12,6 +14,7 @@ def main(cfg: DictConfig) -> None:
     :param cfg: DictConfig configuration composed by Hydra.
     :return: None
     """
+    load_dotenv()  # Load environment variables from .env file
     env, model, optimizer, buffer, train_cfg = instantiate(cfg)
     train(env, model, optimizer, buffer, train_cfg)
 
